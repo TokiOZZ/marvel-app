d7935e4eb830901e29230f7cfbf9078a85d1041d
TokiOZZ <trotsenkop@gmail.com>
2023-01-01 23:59:54 +0200

    155 (PropTypes) - done

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react/cjs/react.development';
+import { PropTypes } from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -119,5 +120,8 @@ class CharList extends Component {
 
 }
 
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
+}
 
 export default CharList;
